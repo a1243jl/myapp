3eef493909f16aaa45454359768d0f6e7fa4a311
chun <chun@chun-virtual-machine.(none)>
2013-01-12 15:12:20 +0900

    generated myapp.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myapp::Application.config.secret_token = 'a7c5e863f8b2201a05fe1f79d960e5259493ad1cf7d0413984963bfd192a42813055df1900664bf1863c5ff117e8b887ee1b9d4c0bb42c05b8912640e8a2a1e0'
